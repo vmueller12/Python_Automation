a088a813edd16fffe246312f996958a237ded349
vmueller12 <mueller.vitali@gmail.com>
2015-10-22 19:45:00 +0100

    Regex phone number

---
+++ Programs/Regex/phoneNumber.py
@@ -0,0 +1,34 @@
+def isPhoneNumber(text): # 415-232-2222
+    if len(text) != 12:
+        return False # Not a phone number size
+    for i in range(0,3):
+        if not text[i].isdecimal():
+            return False # no area code
+
+    if text[3] != '-':
+        return False # missing dash
+
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False # no first 3 digist
+
+    if text[7] != '-':
+        return False # missing second dash
+
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False # missing last 4 digits
+    return True
+
+
+message = "Call me 415-555-1011 tomorrow, or at 425-555-9999 on my office number"
+
+foundNumber = False
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' +chunk)
+        foundNumber = True
+
+if not foundNumber:
+    print('Could not find any phone number.')
