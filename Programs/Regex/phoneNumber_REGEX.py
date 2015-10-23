78d0097239a5b45181c5f0b2ec55e402c42044c0
vmueller12 <mueller.vitali@gmail.com>
2015-10-23 10:30:08 +0100

    Modified version

--- Programs/Regex/phoneNumber_REGEX.py
+++ Programs/Regex/phoneNumber_REGEX.py
@@ -3,10 +3,21 @@ import re
 message = "Call me 415-555-1011 tomorrow, or at 415-555-9999 on my office number"
 
 # Creating Regular Expression Object
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 
 
 match_object = phoneNumRegex.search(message)
 print(phoneNumRegex.findall(message))
 
-print(match_object.group())
+area = match_object.group(1)
+number = match_object.group(2)
+
+print(area)
+print(number)
+
+
+batRegex = re.compile (r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobeile lost a wheel')
+if mo == None:
+    print("No such word pattern")
+#print(mo.group(2))
