4c3308f1a0d2155cd4e10ecd36a3e1f61f13c19c
vmueller12 <mueller.vitali@gmail.com>
2015-10-23 11:05:10 +0100

    more regularar expression patterns

--- Programs/Regex/phoneNumber_REGEX.py
+++ Programs/Regex/phoneNumber_REGEX.py
@@ -21,3 +21,50 @@ mo = batRegex.search('Batmobeile lost a wheel')
 if mo == None:
     print("No such word pattern")
 #print(mo.group(2))
+
+#? 0 or 1 time
+#---------------#
+print('----------------------------------------')
+#batRegex = re.compile(r'Batman|Batwoman')
+batRegex = re.compile(r'(Bat)(wo)?(man)?')
+mo = batRegex.search('The Adventures of Batman')
+print(mo.group())
+
+# * 0 or many more times
+print('-----------------------------------------')
+
+batRegex = re.compile(r'Bat(wo)*man')
+mo = batRegex.search("Batwowowowowowoman")
+print(mo.group())
+
+# + 1 or many more times, can not be 0
+print('------------------------------------------')
+
+batRegex = re.compile(r'Bat(wo)+man')
+if batRegex.search('Batman') == None:
+    print("Wo does not exsist")
+
+
+print('--------------------------------------------')
+# using {3} for getting the group exactly 3 times
+
+batRegex = re.compile(r'(Ha){3}')
+if batRegex.search('HaHa') == None:
+    print('No match')
+
+if batRegex.search('HaHaHa') != None:
+    print('Match')
+
+print(batRegex.search('HameHaHaHa').group())
+
+print('----------------------------------------------')
+
+
+# using {3,5} is a range of repetition 3 timies min on 5 times max
+# greedy matches python will alwys look for the maximum match first
+digitRegex = re.compile(r'(\d){3,5}')
+print(digitRegex.search('12131311').group())
+# now we will implement none greedy match with the smallest possible match
+print('------------------------------------')
+digitRegex = re.compile(r'(\d){3,5}?')
+print(digitRegex.search('1232312312').group())
