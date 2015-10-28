51614082c0d68e7d1f1e9af5a6fdf0220ce9e1b9
vmueller12 <mueller.vitali@gmail.com>
2015-10-28 16:30:56 +0000

    Chunking

---
+++ Programs/NLTK/speech_tagging_nltk.py
@@ -0,0 +1,34 @@
+# This program is for tagging words
+
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+# PunktSentenceTokenizer is an unsupervised machine learning
+# tokenizer is per default pre-trained but can be trained
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try:
+        for i in tokenized[:1]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}"""
+            chunkParser = nltk.RegexpParser(chunkGram)
+            chunked = chunkParser.parse(tagged)
+            print(chunked)
+            #chunked.draw()     
+
+    except Exception as e:
+        print(str(e))
+
+process_content()
+
+
+process_content()
